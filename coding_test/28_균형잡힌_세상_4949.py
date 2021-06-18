3a2ce3f6d0826dbe111befe98e448b057bee9356
SEOLMIN KWON <stat_kwon@naver.com>
2021-06-19 00:01:19 +0900

    feat: graph, bfs, dfs 알고리즘 공부

--- coding_test/28_균형잡힌_세상_4949.py
+++ coding_test/28_균형잡힌_세상_4949.py
@@ -1,4 +1,29 @@
 import sys
-
 sys.stdin = open('./txt/problem_28.txt', 'rt')
 
+
+check_type = ['(', ')', '[', ']', '.']
+while True:
+    stack = []
+    string = input()
+    print(string)
+
+    if string == '.':
+        break
+
+    for i in string:
+        if i in check_type:
+            stack.append(i)
+            if len(stack) == 1:
+                continue
+            if stack[-2] == '(' and stack[-1] == ')':
+                stack.pop()
+                stack.pop()
+                continue
+            if stack[-2] == '[' and stack[-1] == ']':
+                stack.pop()
+                stack.pop()
+    if len(stack) > 1:
+        print('no')
+    else:
+        print('yes')
